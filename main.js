dfc3667211a0e59b4523a7534e762f897872aa36
Amir Afshar <mr.afshar0@gmail.com>
2021-02-08 23:57:32 +0330

    Use the new JS function for expanding one accordion at time

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 // Adding Accordion Functions
 const acc = document.getElementsByClassName("question");
+const panel = document.getElementsByClassName("answer");
 
-for (i=0; i< acc.length; i++){
-    acc[i].addEventListener("click", function(){
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
+// for (i=0; i< acc.length; i++){
+//     acc[i].addEventListener("click", function(){
+//         this.classList.toggle("active");
+//         this.nextElementSibling.classList.toggle("show");
 
-    })
+//     })
+// }
+
+
+for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+        var setClasses = !this.classList.contains("active");
+        setClass(acc, "active", "remove");
+        setClass(panel, "show", "remove");
+
+        if (setClasses) {
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
+    }
+}
+function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+        els[i].classList[fnName](className);
+
+    }
 }
