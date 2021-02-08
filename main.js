fd82f15f9beca06f80c96864692ca8d7c1da7720
Amir Afshar <mr.afshar0@gmail.com>
2021-02-08 14:15:54 +0330

    Removing arrows added with js

--- main.js
+++
@@ -1,8 +0,0 @@
-// Adding Arrow icon to each Question
-document.querySelectorAll(".question").forEach((item) => {
-    var arrow = document.createElement("img");
-    arrow.setAttribute('src', 'images/icon-arrow-down.svg');
-    arrow.setAttribute("class", "arrow");
-    item.appendChild(arrow);
-}
-)
