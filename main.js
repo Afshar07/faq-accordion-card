aad42c0d0fcb92e4ca1d12a9ba6cbf637e73dbbb
Amir Afshar <mr.afshar0@gmail.com>
2021-02-08 16:56:22 +0330

    Simplify JS Function

--- main.js
+++ main.js
@@ -4,15 +4,7 @@ const acc = document.getElementsByClassName("question");
 for (i=0; i< acc.length; i++){
     acc[i].addEventListener("click", function(){
         this.classList.toggle("active");
-
-
-        // Showing and Hiding Answers
-        let panel = this.nextElementSibling;
-        if(panel.style.display === "block"){
-            panel.style.display = "none";
-        }else{
-            panel.style.display = "block";
-        };
+        this.nextElementSibling.classList.toggle("show");
 
     })
 }
